12c86f4965a1018eef369bb5c5b07e2c1077f460
Diego Souza <127131983+DiegoJord@users.noreply.github.com>
2023-09-25 12:42:28 -0300

    commit

---
+++ NotaDeAula6/src/Carro.java
@@ -0,0 +1,30 @@
+public class Carro {
+    private String marca;
+    private String modelo;
+
+    public Carro(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void mostrarDetalhes(){
+        System.out.println("Marca: "+getMarca());
+        System.out.println("Modelo: "+getModelo());
+    }
+}
